7940196e7e0a08f0fd83bffad7617a16ca6ba14d
thrownullpointer <thrownullpointer@gmail.com>
2016-09-04 20:28:49 -0400

    clean up all the dirty code

--- src/main/java/org/jenkinsci/plugins/Confluence/HelloWorldBuilder.java
+++ src/main/java/org/jenkinsci/plugins/Confluence/HelloWorldBuilder.java
@@ -16,10 +16,14 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -64,15 +68,6 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 	private final String name;
 	private final String pageId;
 	private final String filePath;
-	private static final String ENCODING = "utf-8";
-	// private static final String BASE_URL = "http://localhost:8090";
-
-	// Fields in config.jelly must match the parameter names in the
-	// "DataBoundConstructor"
-	/*
-	 * @DataBoundConstructor public HelloWorldBuilder(String name) { this.name =
-	 * name; this.pageId = null; }
-	 */
 
 	@DataBoundConstructor
 	public HelloWorldBuilder(String name, String pageId, String filePath) {
@@ -96,22 +91,12 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 		return filePath;
 	}
 	
-	private static String getContentRestUrl(final Long contentId, final String[] expansions, String u,  String p,  String base) throws UnsupportedEncodingException {
-		final String ENCODING = "utf-8";
-		final String expand = URLEncoder.encode(StringUtils.join(expansions, ","), ENCODING);
-
-		return String.format("%s/rest/api/content/%s?expand=%s&os_authType=basic&os_username=%s&os_password=%s", base, contentId, expand, URLEncoder.encode(u, ENCODING), URLEncoder.encode(p, ENCODING));
-	    }
-
 	@Override
 	public void perform(Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
-		// We can get creds!
-		listener.getLogger().println("Shouldn't we update this page? " + pageId);
-
-		List<UsernamePasswordCredentials> creds = CredentialsProvider
-				.lookupCredentials(UsernamePasswordCredentials.class, Jenkins.getInstance());
+		List<UsernamePasswordCredentials> creds = CredentialsProvider.lookupCredentials(UsernamePasswordCredentials.class, Jenkins.getInstance());
 		UsernamePasswordCredentials c = creds.get(0);
-
+		getDescriptor().getUseFrench();
+		
 		// SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(new File(System.getenv("javax.net.ssl.trustStore")), System.getenv("javax.net.ssl.trustStorePassword").toCharArray(), new TrustSelfSignedStrategy()).build();
 		// Allow TLSv1 protocol only
 		// SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[] { "TLSv1" },null, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
@@ -120,6 +105,17 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 		org.apache.http.client.CredentialsProvider credsProvider = new BasicCredentialsProvider();
 		credsProvider.setCredentials(new AuthScope(target.getHostName(), target.getPort()),
 				new org.apache.http.auth.UsernamePasswordCredentials(c.getUsername(), c.getPassword().getPlainText()));
+		
+		// Create AuthCache instance
+		AuthCache authCache = new BasicAuthCache();
+		// Generate BASIC scheme object and add it to the local auth cache
+		BasicScheme basicAuth = new BasicScheme();
+		authCache.put(target, basicAuth);
+
+		// Add AuthCache to the execution context
+		HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
+		context.setAuthCache(authCache);
 
 		// CloseableHttpClient httpclient =
 		// HttpClients.custom().setSSLSocketFactory(sslsf).setDefaultCredentialsProvider(credsProvider).build();
@@ -130,13 +126,9 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 		HttpEntity pageEntity = null;
 		try {
 			String body = FileUtils.readFileToString(new File(filePath));
-			String uri2 = "http://localhost:8090" + "/rest/api/content/" + pageId;
-			listener.getLogger().println(uri2);
-			String uri = getContentRestUrl(Long.valueOf(pageId), new String[] { "body.storage", "version", "ancestors" }, c.getUsername(), c.getPassword().getPlainText(), "http://localhost:8090");
-			listener.getLogger().println(uri);
-			listener.getLogger().println("HELLO WORLD!");
+			String uri = "http://localhost:8090" + "/rest/api/content/" + pageId;
 			HttpGet getPageRequest = new HttpGet(uri);
-			HttpResponse getPageResponse = httpclient.execute(getPageRequest);
+			HttpResponse getPageResponse = httpclient.execute(target, getPageRequest, context);
 			pageEntity = getPageResponse.getEntity();
 
 			pageObj = IOUtils.toString(pageEntity.getContent());
@@ -154,19 +146,13 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 
 			// Send update request
 			HttpEntity putPageEntity = null;
-
 			HttpPut putPageRequest = new HttpPut(uri);
-
 			StringEntity entity = new StringEntity(page.toString(), ContentType.APPLICATION_JSON);
 			putPageRequest.setEntity(entity);
-
 			HttpResponse putPageResponse = httpclient.execute(putPageRequest);
 			putPageEntity = putPageResponse.getEntity();
-
 			listener.getLogger().println("Put Page Request returned " + putPageResponse.getStatusLine().toString());
-			listener.getLogger().println("");
 			listener.getLogger().println(IOUtils.toString(putPageEntity.getContent()));
-
 			EntityUtils.consume(putPageEntity);
 		} catch (Exception e) {
 			listener.getLogger().print(e);
@@ -174,41 +160,19 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 
 	}
 
-	/*
-	 * private static String getContentRestUrl(final Long contentId, final
-	 * String[] expansions) throws UnsupportedEncodingException { final String
-	 * expand = URLEncoder.encode(StringUtils.join(expansions, ","), ENCODING);
-	 * return String.format("%s/rest/api/content/%s", BASE_URL, contentId); }
-	 * 
-	 * // Overridden for better type safety. // If your plugin doesn't really
-	 * define any property on Descriptor, // you don't have to do this.
-	 * 
-	 * @Override public DescriptorImpl getDescriptor() { return (DescriptorImpl)
-	 * super.getDescriptor(); }
-	 * 
-	 * /** Descriptor for {@link HelloWorldBuilder}. Used as a singleton. The
-	 * class is marked as public so that it can be accessed from views.
-	 *
-	 * <p> See
-	 * <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.
-	 * jelly</tt> for the actual HTML fragment for the configuration screen.
-	 */
-	@Extension // This indicates to Jenkins that this is an implementation of an
-				// extension point.
+ 
+	  // Overridden for better type safety. // If your plugin doesn't really define any property on Descriptor, you don't have to do this.
+	  
+
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
+
+	@Extension
 	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-		/**
-		 * To persist global configuration information, simply store it in a
-		 * field and call save().
-		 *
-		 * <p>
-		 * If you don't want fields to be persisted, use <tt>transient</tt>.
-		 */
 		private boolean useFrench;
 
-		/**
-		 * In order to load the persisted global configuration, you have to call
-		 * load() in the constructor.
-		 */
 		public DescriptorImpl() {
 			load();
 		}
@@ -242,14 +206,9 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 		}
 
 		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-			// Indicates that this builder can be used with all kinds of project
-			// types
 			return true;
 		}
 
-		/**
-		 * This human readable name is used in the configuration screen.
-		 */
 		public String getDisplayName() {
 			return "Confluence Hook";
 		}
